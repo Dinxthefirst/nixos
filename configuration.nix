1a9169b98626dd0462c1bc83005febcbee9dcaf5
Oliver Toft <olivertoftk@live.dk>
2025-03-18 23:32:09 +0100

    NixOS Generation: 16 current  2025-03-18 23:32:08  25.05.20250313.6607cf7     6.12.18                          *

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,8 @@
     ./packages.nix
   ];
 
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   # Set your time zone.
   time.timeZone = "Europe/Copenhagen";
 
@@ -40,8 +42,6 @@
   # Configure console keymap
   console.keyMap = "dk-latin1";
 
-  programs.zsh.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.toft = {
     isNormalUser = true;
@@ -51,37 +51,12 @@
     shell = pkgs.zsh;
   };
 
+  programs.zsh.enable = true;
+
   hardware.graphics.enable = true;
 
   xdg.portal = {
     enable = true;
     extraPortals = [pkgs.xdg-desktop-portal-gtk];
   };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
 }
