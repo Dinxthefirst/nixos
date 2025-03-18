4de1880cd8af08504552d9fa9bd663be8bce18f8
Oliver Toft <olivertoftk@live.dk>
2025-03-18 21:58:21 +0100

    13 current  2025-03-18 21:45:29  25.05.20250313.6607cf7     6.12.18                          *

--- home.nix
+++ home.nix
@@ -30,9 +30,9 @@
     enable = true;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
-      brettm12345.nixfmt-vscode
       ms-vsliveshare.vsliveshare
       james-yu.latex-workshop
+      esbenp.prettier-vscode
     ];
   };
 }
