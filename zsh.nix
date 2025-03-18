06119c37b894076fe35a4138a34ccd859f5cc773
Oliver Toft <olivertoftk@live.dk>
2025-03-19 00:05:37 +0100

    NixOS Generation: 21 current  2025-03-19 00:05:36  25.05.20250313.6607cf7     6.12.18                          *

--- zsh.nix
+++ zsh.nix
@@ -5,34 +5,6 @@
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
 
-    shellInit = ''
-      fastfetch -c ~/.config/fastfetch/config.json -l nixos_small
-
-      autoload -U colors && colors
-
-      precmd() {
-        print -P "%{$fg_bold[blue]%}%~%{$reset_color%}"
-      }
-
-      PROMPT="> "
-
-      autoload -Uz compinit
-      compinit
-
-      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-      bindkey '^[[1;5D' backward-word
-      bindkey '^[[1;5C' forward-word
-      bindkey '^[[3~' delete-char
-      bindkey '^[[3;5~' kill-word
-      bindkey '^H' backward-kill-word
-
-      mkcd() = mkdir -p "$1" | cd "$1"
-      pdf() = zen-browser "$1" &
-    '';
-
-    # history.size = 10000;
-
     shellAliases = {
       config = "code ~/.config/nixos";
       rebuild = "~/rebuild.sh";
