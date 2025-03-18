c05a1c9a9f24ddffb0468e5ef49489725c6d1fd9
Oliver Toft <olivertoftk@live.dk>
2025-03-18 22:26:44 +0100

    NixOS Generation: 15 current  2025-03-18 22:26:43  25.05.20250313.6607cf7     6.12.18                          *

--- gnome.nix
+++ gnome.nix
@@ -8,15 +8,34 @@
     gnome-tweaks
   ];
   environment.gnome.excludePackages = with pkgs; [
-    epiphany
-    evince
-    geary
-    gedit
+    baobab # disk usage analyzer
+    cheese # photo booth
+    eog # image viewer
+    epiphany # web browser
+    gedit # text editor
+    simple-scan # document scanner
+    totem # video player
+    yelp # help viewer
+    evince # document viewer
+    file-roller # archive manager
+    geary # email client
+    seahorse # password manager
+
+    # these should be self explanatory
+    gnome-calculator
+    gnome-calendar
     gnome-characters
+    gnome-clocks
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
     gnome-music
     gnome-photos
-    gnome-terminal
-    gnome-tour
-    totem
+    gnome-screenshot
+    gnome-system-monitor
+    gnome-weather
+    gnome-disk-utility
+    pkgs.gnome-connections
   ];
 }
