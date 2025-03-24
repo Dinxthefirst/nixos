52852f21715d2391933a4f7bcb5ac86c68bb12ad
Oliver Toft <olivertoftk@live.dk>
2025-03-24 11:42:26 +0100

    NixOS Generation: 29

--- hyprland.nix
+++ hyprland.nix
@@ -10,8 +10,11 @@
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
   };
 
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
 
   environment.systemPackages = with pkgs; [
     waybar
