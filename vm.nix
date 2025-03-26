9995b6f44edf70baf927a0e6a2b3656792e9b66b
Oliver Toft <olivertoftk@live.dk>
2025-03-26 16:48:13 +0100

    NixOS Generation: 62

--- vm.nix
+++ vm.nix
@@ -1,23 +1,51 @@
-{...}: {
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
-
-  # Required Kernel Modules
-  boot.kernelModules = [
-    "kvm-amd"
-    "vfio"
-    "vfio_pci"
-    "vfio_iommu_type1"
-    "vfio_virqfd"
+{
+  pkgs,
+  config,
+  ...
+}: let
+  # Change this to your username.
+  user = "toft";
+  # Change this to match your system's CPU.
+  platform = "amd";
+  # Change this to specify the IOMMU ids you wrote down earlier.
+  vfioIds = ["1002:744c" "1002:ab30"];
+in {
+  # Configure kernel options to make sure IOMMU & KVM support is on.
+  boot = {
+    kernelModules = ["kvm-${platform}" "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio"];
+    kernelParams = ["${platform}_iommu=on" "${platform}_iommu=pt" "kvm.ignore_msrs=1"];
+    extraModprobeConfig = "options vfio-pci ids=${builtins.concatStringsSep "," vfioIds}";
+  };
+  # Add a file for looking-glass to use later. This will allow for viewing the guest VM's screen in a
+  # performant way.
+  systemd.tmpfiles.rules = [
+    "f /dev/shm/looking-glass 0660 ${user} qemu-libvirtd -"
   ];
-
-  # Enable IOMMU & PCI passthrough
-  boot.kernelParams = [
-    "amd_iommu=on"
-    "iommu=pt"
-    "vfio-pci.ids=1002:744c,1002:ab30"
+  # Add virt-manager and looking-glass to use later.
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    looking-glass-client
   ];
-
-  # Assign user to libvirt & KVM groups
-  users.users.toft.extraGroups = ["libvirtd" "kvm"];
+  # Enable virtualisation programs. These will be used by virt-manager to run your VM.
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      extraConfig = ''
+        user="${user}"
+      '';
+      # Don't start any VMs automatically on boot.
+      onBoot = "ignore";
+      # Stop all running VMs on shutdown.
+      onShutdown = "shutdown";
+      qemu = {
+        package = pkgs.qemu_kvm;
+        # ovmf = "enabled";
+        verbatimConfig = ''
+           namespaces = []
+          user = "+${builtins.toString config.users.users.${user}.uid}"
+        '';
+      };
+    };
+  };
+  users.users.${user}.extraGroups = ["qemu-libvirtd" "libvirtd" "disk"];
 }
