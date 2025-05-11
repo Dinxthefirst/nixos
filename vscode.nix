bc490e88fe9db5c68e9146f940722aaed7ffc751
Oliver Toft <olivertoftk@live.dk>
2025-05-11 21:16:37 +0200

    NixOS Generation: 93

--- vscode.nix
+++ vscode.nix
@@ -14,7 +14,28 @@
         ms-python.vscode-pylance
       ];
       userSettings = {
-        "[html, nix, ocaml, json, css, javascript, jsonc, typescript]" = {
+        "[html]" = {
+          "editor.tabSize" = 2;
+        };
+        "[nix]" = {
+          "editor.tabSize" = 2;
+        };
+        "[ocaml]" = {
+          "editor.tabSize" = 2;
+        };
+        "[json]" = {
+          "editor.tabSize" = 2;
+        };
+        "[css]" = {
+          "editor.tabSize" = 2;
+        };
+        "[javascript]" = {
+          "editor.tabSize" = 2;
+        };
+        "[jsonc]" = {
+          "editor.tabSize" = 2;
+        };
+        "[typescript]" = {
           "editor.tabSize" = 2;
         };
         "latex-workshop.latex.outDir" = "/tmp/latex-build";
