4bd487f1e3f4a583286d8b3ea3979ab0e89f8c04
Oliver Toft <olivertoftk@live.dk>
2025-05-30 15:43:23 +0200

    NixOS Generation: 114

--- home/programs/alacritty/default.nix
+++ home/programs/alacritty/default.nix
@@ -1,47 +1,37 @@
 {
-  programs.alacritty.settings = {
+  programs.alacritty = {
     enable = true;
-    window.dimensions = {
-      lines = 3;
-      columns = 200;
+    settings = {
+      font = {
+        size = 16.0;
+        normal = {
+          family = "Fira Code";
+          style = "Regular";
+        };
+        bold = {
+          family = "Fira Code";
+          style = "Bold";
+        };
+        italic = {
+          family = "Fira Code";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "Fira Code";
+          style = "Bold Italic";
+        };
+      };
+
+      window = {
+        opacity = 0.7;
+      };
+
+      colors = {
+        primary = {
+          background = "#000000";
+          foreground = "#FFFFFF";
+        };
+      };
     };
-    keyboard.bindings = [
-      {
-        key = "K";
-        mods = "Control";
-        chars = "\\u000c";
-      }
-    ];
-
-    # font = {
-    #   size = 16.0;
-    #   normal = {
-    #     family = "Fira Code";
-    #     style = "Regular";
-    #   };
-    #   bold = {
-    #     family = "Fira Code";
-    #     style = "Bold";
-    #   };
-    #   italic = {
-    #     family = "Fira Code";
-    #     style = "Italic";
-    #   };
-    #   bold_italic = {
-    #     family = "Fira Code";
-    #     style = "Bold Italic";
-    #   };
-    # };
-
-    # window = {
-    #   opacity = 0.7;
-    # };
-
-    # colors = {
-    #   primary = {
-    #     background = "#000000";
-    #     foreground = "#FFFFFF";
-    #   };
-    # };
   };
 }
