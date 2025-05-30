2191a757ef9ee42f4b65938e30a2f983ea85e865
Oliver Toft <olivertoftk@live.dk>
2025-05-30 15:52:12 +0200

    NixOS Generation: 116

--- fonts.nix
+++ fonts.nix
@@ -1,7 +1,8 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
-  fonts.packages = with pkgs; [fira-code font-awesome];
+{pkgs, ...}: {
+  fonts.packages = with pkgs; [
+    # fira-code
+    # nerd-fonts-fira-code
+    # font-awesome
+    nerd-fonts.fira-code
+  ];
 }
