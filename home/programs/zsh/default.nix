04691a347598eaa433f3e5813015342cfbd597c8
Oliver Toft <olivertoftk@live.dk>
2025-05-31 22:10:39 +0200

    NixOS Generation: 8

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -19,6 +19,22 @@
       la = "ls -a --color=auto";
     };
 
+    initExtra = ''
+      fastfetch -c ~/.config/fastfetch/config.json -l nixos_small
+
+      autoload -U colors && colors
+
+      precmd() {
+        print -P "%{$fg_bold[blue]%}%~%{$reset_color%}"
+      }
+
+      bindkey '^[[1;5D' backward-word
+      bindkey '^[[1;5C' forward-word
+      bindkey '^[[3~' delete-char
+      bindkey '^[[3;5~' kill-word
+      bindkey '^H' backward-kill-word
+    '';
+
     history = {
       save = 1000;
       size = 1000;
