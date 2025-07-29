0fbf2c09de03a9aef776bb1c7898c566440b859a
Oliver Toft <olivertoftk@live.dk>
2025-07-29 12:53:22 +0200

    NixOS Generation: 45

--- hosts/desktop/user.nix
+++ hosts/desktop/user.nix
@@ -3,24 +3,33 @@
     ../../modules
   ];
 
-  config.modules = {
-    zen.enable = true;
-    flatpak.enable = true;
-    gnome.enable = true;
-    discord.enable = true;
-    steam.enable = true;
-    stremio.enable = true;
-    gaming.enable = true;
-    latex.enable = true;
-    nodejs.enable = true;
-    git.enable = true;
-    zsh.enable = true;
-    vscode.enable = true;
-    direnv.enable = true;
-    obsidian.enable = true;
-    minecraft.enable = true;
-    gparted.enable = true;
-    vm.enable = false;
-    godot.enable = true;
+  config = {
+    hardware.opengl = {
+      enable = true;
+      driSupport32Bit = true;
+    };
+    environment.variables = {
+      ROC_ENABLE_PRE_VEGA = "1";
+    };
+    modules = {
+      zen.enable = true;
+      flatpak.enable = true;
+      gnome.enable = true;
+      discord.enable = true;
+      steam.enable = true;
+      stremio.enable = true;
+      gaming.enable = true;
+      latex.enable = true;
+      nodejs.enable = true;
+      git.enable = true;
+      zsh.enable = true;
+      vscode.enable = true;
+      direnv.enable = true;
+      obsidian.enable = true;
+      minecraft.enable = true;
+      gparted.enable = true;
+      vm.enable = false;
+      godot.enable = true;
+    };
   };
 }
