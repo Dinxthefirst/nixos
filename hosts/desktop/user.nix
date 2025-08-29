e67d04af91a7b595b2df06fda42a8a55adc51ccf
Oliver Toft <olivertoftk@live.dk>
2025-08-29 19:05:41 +0200

    NixOS Generation:

--- hosts/desktop/user.nix
+++ hosts/desktop/user.nix
@@ -10,7 +10,7 @@
       gnome.enable = true;
       discord.enable = true;
       steam.enable = true;
-      stremio.enable = false;
+      stremio.enable = true;
       gaming.enable = true;
       latex.enable = true;
       nodejs.enable = false;
