d8ad4e8d1f02941af5e94b249129c1cb276c1a7e
Oliver Toft <olivertoftk@live.dk>
2025-11-15 20:09:03 +0100

    NixOS Generation: laptop 112

--- modules/neovim/lua/options.lua
+++ modules/neovim/lua/options.lua
@@ -1,3 +1,8 @@
 vim.opt.relativenumber = true
 vim.opt.number = true 
 vim.opt.scrolloff = 10 
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
