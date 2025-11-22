dacada6290983b64a84552b7fbedfaba8d6da458
Oliver Toft <olivertoftk@live.dk>
2025-11-22 23:36:00 +0100

    NixOS Generation: desktop 92

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -78,6 +78,9 @@ in {
           switch-to-workspace-5 = ["<Super>5"];
           close = ["<Super>q"];
         };
+        "org/gnome/desktop/input-sources" = {
+          xkb-options = ["lv3:lalt_switch"];
+        };
         # "org/gnome/settings-daemon/plugins/media-keys" = {
         #   custom-keybindings = [
         #     "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
