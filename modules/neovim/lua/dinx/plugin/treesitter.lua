00bcc01492abe605da0aebf278b4e01744b4542d
Oliver Toft <olivertoftk@live.dk>
2025-11-23 22:26:40 +0100

    NixOS Generation: desktop 100

--- modules/neovim/lua/dinx/plugin/treesitter.lua
+++ modules/neovim/lua/dinx/plugin/treesitter.lua
@@ -18,4 +18,6 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+
+  parser_install_dir = vim.fn.stdpath("data") .. "/nvim-treesitter/parsers",
 }
