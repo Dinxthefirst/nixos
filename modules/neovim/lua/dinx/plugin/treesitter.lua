52c6e634ad43d0c3aeed7b3c750f6e7c9b297417
Oliver Toft <olivertoftk@live.dk>
2025-11-23 22:32:46 +0100

    NixOS Generation: desktop 103

--- modules/neovim/lua/dinx/plugin/treesitter.lua
+++ modules/neovim/lua/dinx/plugin/treesitter.lua
@@ -1,7 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = { 
-    "svelte", 
     "javascript", 
     "typescript", 
     "c", 
