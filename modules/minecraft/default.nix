a4147b4274785670df2d6558c75bbc1e576d8069
Oliver Toft <olivertoftk@live.dk>
2025-12-09 20:34:34 +0100

    NixOS Generation: desktop 110

--- modules/minecraft/default.nix
+++ modules/minecraft/default.nix
@@ -14,7 +14,7 @@ in {
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       prismlauncher-unwrapped
-      openjdk
+      javaPackages.compiler.openjdk25
     ];
   };
 }
