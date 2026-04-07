1b26626fb7586939778d2c9be11545636e076d42
Oliver Toft <olivertoftk@live.dk>
2026-04-07 21:52:55 +0200

    NixOS Generation: desktop 188

--- modules/hyprland/waybar.nix
+++ modules/hyprland/waybar.nix
@@ -69,13 +69,13 @@ in {
             calendar = {
               mode = "year";
               mode-mon-col = 3;
-              weeks-pos = "left";
+              # weeks-pos = "left";
               on-scroll = 1;
               on-click-right = "mode";
               format = {
                 months = "<span color='#ffead3'><b>{}</b></span>";
                 days = "<span color='#ecc6d9'><b>{}</b></span>";
-                weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+                # weeks = "<span color='#99ffdd'><b>{}</b></span>";
                 weekdays = "<span color='#ffcc66'><b>{}</b></span>";
                 today = "<span color='#ff6699'><b><u>{}</u></b></span>";
               };
