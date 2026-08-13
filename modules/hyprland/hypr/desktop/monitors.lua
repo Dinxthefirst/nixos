7b617dc9f1470bb598723a60e9ad4a5a71c71258
Oliver Toft <olivertoftk@live.dk>
2026-08-13 09:04:06 +0200

    nixos: desktop generation 332

--- modules/hyprland/hypr/desktop/monitors.lua
+++ modules/hyprland/hypr/desktop/monitors.lua
@@ -1,13 +1,13 @@
 hl.monitor({
   output = "DP-1",
-  mode = "2560x1440@165.00",
+  mode = "2560x1440@165",
   position = "0x0",
   scale = 1,
 })
 
 hl.monitor({
   output = "HDMI-A-1"
-  mode = "1920x1080@60.00",
+  mode = "1920x1080@60",
   position = "2560x360",
   scale = 1,
 })
