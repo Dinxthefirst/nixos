136452d3034f805b54ac1c3c8b4cfd58708efc88
Oliver Toft <olivertoftk@live.dk>
2026-08-14 09:24:29 +0200

    nixos: laptop generation 211

--- modules/hyprland/hypr/laptop/input.lua
+++ modules/hyprland/hypr/laptop/input.lua
@@ -14,5 +14,5 @@ hl.config({
 hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace"})
 
 -- hl.gesture({ fingers = 2, direction = "pinch", action = "cursorZoom", zoom_level = 2 })
--- hl.gesture({ fingers = 2, direction = "pinch", action = "cursorZoom", zoom_level = 1.2, mode = "mult" })
-hl.gesture({ fingers = 2, direction = "pinch", action = "cursorZoom", zoom_level = 1, mode = "live" })
+hl.gesture({ fingers = 2, direction = "pinch", action = "cursorZoom", zoom_level = 1.2, mode = "mult" })
+-- hl.gesture({ fingers = 2, direction = "pinch", action = "cursorZoom", zoom_level = 1, mode = "live" })
