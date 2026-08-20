53cd35c106c80ca54e47b95cf109e63d8769d5ae
Oliver Toft <olivertoftk@live.dk>
2026-08-20 13:14:47 +0200

    nixos: desktop generation 341

--- modules/steam/default.nix
+++ modules/steam/default.nix
@@ -7,6 +7,12 @@
 }:
 with lib; let
   cfg = config.modules.steam;
+  creamlinux = import (pkgs.fetchFromGitHub {
+    owner = "Novattz";
+    repo = "creamlinux-installer";
+    rev = "main"; # replace with a commit hash to pin the version
+    hash = "sha256-sV23mp0XnJHf4oSqqvFLFfvSkssHzxafqYMNw3HGEdg="; # paste the value returned by the error your rebuild will output
+  }) {inherit pkgs;};
 in {
   options = {
     modules.steam.enable = mkEnableOption "steam";
@@ -23,8 +29,8 @@ in {
 
     environment.systemPackages = with pkgs; [
       mangohud
-      scarab # hollow knight mod manager
-      # inputs.creamlinux.packages.${pkgs.system}.default
+      # scarab # hollow knight mod manager
+      creamlinux
     ];
 
     programs.gamemode.enable = true;
